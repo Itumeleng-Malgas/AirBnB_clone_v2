4bddd50a5e2236ee76299279c5533af181d33c88
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-01-11 18:50:05 +0200

    versions/web_static_20240111164809.tgz

---
+++ 1-pack_web_static.py
@@ -0,0 +1,31 @@
+""" enerate a .tgz archive module """
+from fabric.api import local
+from datetime import datetime
+import os
+
+def do_pack():
+    """
+    Generate a .tgz archive from the contents of the web_static folder.
+
+    Returns:
+        str: Archive path if generated successfully, None otherwise.
+    """
+    # Create the versions folder if it doesn't exist
+    if not os.path.exists("versions"):
+        local("mkdir -p versions")
+
+    # Create the archive name using the current date and time
+    timestamp = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+    archive_name = "web_static_{}.tgz".format(timestamp)
+
+    # Create the tar command
+    tar_command = "tar -czvf versions/{} web_static".format(archive_name)
+
+    # Execute the tar command
+    result = local(tar_command)
+
+    # Check if the archive was created successfully
+    if result.succeeded:
+        return os.path.join("versions", archive_name)
+    else:
+        return None
