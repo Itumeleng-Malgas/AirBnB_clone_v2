29cdd76396169096a2826f18b91767979b5847a9
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-01-12 04:07:04 +0200

    3-deploy_web_static.py: pycodestyle fix

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -34,17 +34,20 @@ def do_deploy(archive_path):
         # Upload the archive to the /tmp/ directory
         put(archive_path, "/tmp/")
 
-        # Extract the archive to /data/web_static/releases/<archive filename without extension>
+        # Extract the archive to /data/web_static/releases/<archive filename>
         file_name = archive_path.split('/')[-1]
         folder_name = file_name.split('.')[0]
         run("mkdir -p /data/web_static/releases/{}".format(folder_name))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file_name, folder_name))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(file_name, folder_name))
 
-        # Move contents to /data/web_static/releases/<archive filename without extension>
-        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(folder_name, folder_name))
+        # Move contents to /data/web_static/releases/<archive filename>
+        run("mv /data/web_static/releases/{}/web_static/* "
+            "/data/web_static/releases/{}/".format(folder_name, folder_name))
 
         # Delete the extracted folder
-        run("rm -rf /data/web_static/releases/{}/web_static".format(folder_name))
+        run("rm -rf /data/web_static/releases/{}/web_static"
+            .format(folder_name))
 
         # Delete the archive from the web server
         run("rm /tmp/{}".format(file_name))
@@ -53,7 +56,8 @@ def do_deploy(archive_path):
         run("rm -rf /data/web_static/current")
 
         # Create a new symbolic link /data/web_static/current
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(folder_name))
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(folder_name))
 
         return True
 
