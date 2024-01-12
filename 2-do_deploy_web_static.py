5c292b211eee91902b124ca92956e2fe75075657
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-01-12 03:48:46 +0200

    2-do_deploy_web_static.py_fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,10 +17,10 @@ def do_deploy(archive_path):
         return False
 
     try:
-        # Upload the archive to the /tmp/ directory of the web server
+        # Upload the archive to the /tmp/ directory
         put(archive_path, "/tmp/")
 
-        # Extract the archive to the folder /data/web_static/releases/<archive
+        # Extract the archive to /data/web_static/releases/<archive
         # filename without extension>
         file_name = archive_path.split('/')[-1]
         folder_name = file_name.split('.')[0]
@@ -28,19 +28,27 @@ def do_deploy(archive_path):
         run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
             .format(file_name, folder_name))
 
+        # Move contents to /data/web_static/releases/<archive
+        # filename without extension>
+        run("mv /data/web_static/releases/{}/web_static/* "
+            "/data/web_static/releases/{}/".format(folder_name, folder_name))
+
+        # Delete the extracted folder
+        run("rm -rf /data/web_static/releases/{}/web_static"
+            .format(folder_name))
+
         # Delete the archive from the web server
         run("rm /tmp/{}".format(file_name))
 
-        # Delete the symbolic link /data/web_static/current from the web server
+        # Delete the symbolic link /data/web_static/current
         run("rm -rf /data/web_static/current")
 
         # Create a new symbolic link /data/web_static/current
         run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
             .format(folder_name))
 
-        print("New version deployed!")
         return True
 
     except Exception as e:
-        print(e)
+        print("[{}] {}".format(env.host, e))
         return False
