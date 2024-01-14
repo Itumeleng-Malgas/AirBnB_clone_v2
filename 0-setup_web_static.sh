25ce6ada73cd3d525810b659acdeef36239848d1
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-01-14 12:18:10 +0200

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,22 +1,29 @@
 #!/usr/bin/env bash
 # Sets up webservers for deployment of web_static
 
-# Check if nginx is installed, and install it if not
+# Install nginx if not installed
 if ! command -v nginx &> /dev/null; then
     apt -y update
     apt -y install nginx
-    ufw allow 'Nginx HTTP'
 fi
 
-# Create necessary directories
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
+ufw allow 'Nginx HTTP'
+
+# Directories
+mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
+
+# HTML content
+html_content="<!DOCTYPE html><html><body>Hello world!</body></html>"
 
 # Create a simple HTML file for testing
-echo "Hello world!" | sudo tee /data/web_static/releases/test/index.html
+echo -e "$html_content" | sudo tee /data/web_static/releases/test/index.html
 
 # Create a symbolic link to the 'test' release
-ln -sf /data/web_static/releases/test/ /data/web_static/current/
+target_directory="/data/web_static/current/"
+if [ ! -d "$target_directory" ]; then
+    mkdir -p "$target_directory"
+fi
+ln -sf /data/web_static/releases/test/ "$target_directory"
 
 # Ensure correct permissions
 chown -R ubuntu:ubuntu /data/
@@ -25,7 +32,7 @@ chown -R ubuntu:ubuntu /data/
 config_str=$'\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n'
 sed -i "/server_name .*;/a$config_str" /etc/nginx/sites-available/default
 
-# Create a symbolic link to enable the default site configuration
+# Enable Nginx site configuration
 ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled
 
 # Reload Nginx to apply changes
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,12 +3,11 @@
 Fabric script that distributes an archive to your web servers
 """
 
-from fabric.api import env, put, run, local
+from fabric.api import env, put, run
 from os.path import exists
 
-env.hosts = ['3.85.148.189', '54.87.195.116']
-
-
+env.hosts = ['web-01.botlhaledev.tech', 'web-02.botlhaledev.tech']
+releases_path = '/data/web_static/releases'
 
 def do_deploy(archive_path):
     """
@@ -16,22 +15,31 @@ def do_deploy(archive_path):
     """
     if not exists(archive_path):
         return False
+    
     try:
+        # Upload the archive to the /tmp/ directory on the server
         put(archive_path, "/tmp/")
+
         file_name = archive_path.split('/')[-1]
         folder_name = file_name.split('.')[0]
-        run("mkdir -p /data/web_static/releases/{}".format(folder_name))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file_name, folder_name))
+        release_folder = "{}/{}".format(releases_path, folder_name)
 
-        run("mv /data/web_static/releases/{}/web_static/* "
-            "/data/web_static/releases/{}/".format(folder_name, folder_name))
+        run("mkdir -p {}".format(release_folder))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, release_folder))
 
-        run("rm -rf /data/web_static/releases/{}/web_static".format(folder_name))
+        # Move the contents of the web_static folder to the parent folder
+        run("mv {}/web_static/* {}/".format(release_folder, release_folder))
+
+        # Remove unnecessary directories and files
+        run("rm -rf {}/web_static".format(release_folder))
         run("rm /tmp/{}".format(file_name))
+
+        # Update the symbolic link
         run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(folder_name))
+        run("ln -s {} /data/web_static/current".format(release_folder))
+
         return True
+
     except Exception as e:
         print("[{}] {}".format(env.host, e))
         return False
