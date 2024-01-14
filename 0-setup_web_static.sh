42e2c7564e45ec854bf0f319c6a19b1c591e9e20
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-01-14 07:27:09 +0200

    Reduced number of lines

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,8 +2,7 @@
 # Sets up webservers for deployment of web_static
 
 # Install nginx if it is not installed already
-apt -y update
-apt -y install nginx
+apt -y update && apt -y install nginx
 
 ufw allow 'Nginx HTTP'
 
@@ -15,23 +14,18 @@ mkdir -p /data/web_static/shared
 mkdir -p /data/web_static/releases/test
 
 # Create a fake HTML file
-html_content="<html><head></head><body>Holberton School</body></html>"
-echo "$html_content" | sudo tee /data/web_static/releases/test/index.html
-
-# Symbolic link /data/web_static/current to /data/web_static/releases/test
+echo "Hello world!" | sudo tee /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # Giving ownership to user ubuntu
 chown -R ubuntu:ubuntu /data/
 
-# Serve the content of /data/web_static/current/ at /hbnb_static
 conf="
 \tlocation /hbnb_static {
 \t\talias /data/web_static/current/;
 \t}
 "
 sed -i "/servername .*;/a\ $conf" /etc/nginx/sites-available/default
-
-# Link site-available with site-enabled
 ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled
+
 service nginx restart
