bb9f982a32d548012288254005d164ca949e7a7d
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-01-14 17:47:28 +0200

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,35 +1,25 @@
 #!/usr/bin/env bash
 # Sets up webservers for deployment of web_static
 
-# Install nginx if not installed
 if ! command -v nginx &> /dev/null; then
     apt -y update
     apt -y install nginx
     ufw allow 'Nginx HTTP'
 fi
 
-# Directories
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 
-# HTML content
 html_content="<!DOCTYPE html><html><body>Hello world!</body></html>"
-
-# Create a simple HTML file for testing
-echo -e "$html_content" | sudo tee /data/web_static/releases/test/index.html
-
-ln -sf /data/web_static/releases/test/ /data/web_static/current/
-
-# Ensure correct permissions
-chown -R ubuntu:ubuntu /data/
+echo -e "$html_content" | tee /data/web_static/releases/test/index.html
 
 if [ -d "/data/web_static/current/" ]; then
     rm -rf /data/web_static/current/
 fi
+
 ln -sf /data/web_static/releases/test/ /data/web_static/current/
+chown -R ubuntu:ubuntu /data/
 
-# Enable Nginx site configuration
-ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled
+sudo sed -i '/listen 80 default_server/a location /hbnb_static{ alias /data/web_static/current;}' /etc/nginx/sites-enabled/default
 
-# Reload Nginx to apply changes
-service nginx reload
+service nginx restart
