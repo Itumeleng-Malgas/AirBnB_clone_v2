e462fdef61b7c724ca7021c62c7177e3360a5042
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-01-14 07:37:59 +0200

    Reduced number of lines to 22

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
 # Sets up webservers for deployment of web_static
 
-# Install nginx if it is not installed already
 apt -y update && apt -y install nginx
-
 ufw allow 'Nginx HTTP'
 
 # Create necessary directories
-mkdir -p /data/
-mkdir -p /data/web_static/
-mkdir -p /data/web_static/releases/
-mkdir -p /data/web_static/shared
 mkdir -p /data/web_static/releases/test
+mkdir -p /data/web_static/shared
 
 # Create a fake HTML file
 echo "Hello world!" | sudo tee /data/web_static/releases/test/index.html
@@ -20,12 +15,8 @@ ln -sf /data/web_static/releases/test/ /data/web_static/current
 # Giving ownership to user ubuntu
 chown -R ubuntu:ubuntu /data/
 
-conf="
-\tlocation /hbnb_static {
-\t\talias /data/web_static/current/;
-\t}
-"
-sed -i "/servername .*;/a\ $conf" /etc/nginx/sites-available/default
-ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled
+config_str="\n\tlocation /hbnb_static {\n\t\t alias /data/web_static/current/;\n\t}"
+sed -i "/servername .*;/a\ $config_str" /etc/nginx/sites-available/default
 
+ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled
 service nginx restart
