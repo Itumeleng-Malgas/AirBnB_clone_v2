9ec50ef266410a2e5c0ef60fa9260273cd40214d
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-01-14 18:51:23 +0200

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -26,14 +26,14 @@ def do_deploy(archive_path):
         release_folder = "{}/{}".format(releases_path, folder_name)
 
         run("mkdir -p {}".format(release_folder))
-        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_name))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, release_folder))
 
         # Move the contents of the web_static folder to the parent folder
-        run("mv /tmp/web_static/* {}".format(release_folder))
+        run("mv {}/web_static/* {}".format(release_folder, release_folder))
 
         # Remove unnecessary directories and files
         run("rm /tmp/{}".format(file_name))
-        run("rm -rf /tmp/web_static"
+        run("rm -rf /tmp/web_static")
 
         # Update the symbolic link
         run("rm -rf /data/web_static/current")
