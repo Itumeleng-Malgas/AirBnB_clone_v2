26e78715c92eb0c3e2c71ebb79f820d4562aa515
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-01-14 18:47:30 +0200

    2-do_deploy_web_static.py_my_stuff

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,6 +7,7 @@ from fabric.api import env, put, run
 from os.path import exists
 
 env.hosts = ['3.85.148.189', '54.87.195.116']
+env.user = "ubuntu"
 releases_path = '/data/web_static/releases'
 
 
@@ -25,14 +26,14 @@ def do_deploy(archive_path):
         release_folder = "{}/{}".format(releases_path, folder_name)
 
         run("mkdir -p {}".format(release_folder))
-        run("tar -xzf /tmp/{} -C {}".format(file_name, release_folder))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_name))
 
         # Move the contents of the web_static folder to the parent folder
-        run("mv {}/web_static/* {}/".format(release_folder, release_folder))
+        run("mv /tmp/web_static/* {}".format(release_folder))
 
         # Remove unnecessary directories and files
-        # run("rm -rf {}/web_static".format(release_folder))
         run("rm /tmp/{}".format(file_name))
+        run("rm -rf /tmp/web_static"
 
         # Update the symbolic link
         run("rm -rf /data/web_static/current")
