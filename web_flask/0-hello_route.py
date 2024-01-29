1db1cb40b6a87fc1c8f0e292c277af6e40f08dbe
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-01-29 10:22:56 +0200

    pycodestyle fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,10 +5,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """ Function that return Hello HBNB """
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     # Run the application on 0.0.0.0 and port 5000
