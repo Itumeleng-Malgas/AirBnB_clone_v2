0c8699bdadf6a607402818fcbda8a5abd1ee7510
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-01-29 11:12:21 +0200

    4-number_route.py

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" Script that starts a Flask web application. """
+
+from flask import Flask, escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Function that return Hello HBNB """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Function that return HBNB """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """ Return "C " followed by the value of the text variable """
+    text = escape(text).replace('_', ' ')
+    return f'C {text}'
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text='is cool'):
+    """ Return  "Python ", followed by the value of the text """
+    text = escape(text).replace('_', ' ')
+    return f'Python {text}'
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """  display "n is a number" only if n is an integer """
+    return f'{n} is a number'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
